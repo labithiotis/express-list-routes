5e28592fa2f377447a70889aa99207ade284101e
Kamal Yusuf <kbomoi@yahoo.com>
2023-03-20 05:17:09 +0300

    added typescript types

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+interface Options {
+  spacer?: number;
+  prefix?: string;
+  logger?: (method: string, space: string, path: string) => void;
+}
+
+interface Route {
+  method?: string;
+  path?: string;
+}
+
+interface Stack {
+  route?: {
+    stack: Route[];
+    path?: string;
+  };
+  routerPath?: string;
+}
+
+interface ExpressApp {
+  _router: {
+    stack: Stack[];
+  };
+}
+
+declare function expressListRoutes(app: ExpressApp, options?: Options): void;
+
+export { expressListRoutes as default };
