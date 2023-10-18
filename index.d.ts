18a8ff6bcc2407d6cdaf6062d2000349db1c9e66
yutak23 <79501292+yutak23@users.noreply.github.com>
2023-10-18 16:48:12 +0900

    fix: type for supporting Express 4 router and so on (#21)

--- index.d.ts
+++ index.d.ts
@@ -2,27 +2,30 @@ interface Options {
   spacer?: number;
   prefix?: string;
   logger?: (method: string, space: string, path: string) => void;
+  color?: boolean;
 }
 
 interface Route {
-  method?: string;
+  method?: string | object;
   path?: string;
 }
 
-interface Stack {
-  route?: {
-    stack: Route[];
-    path?: string;
-  };
-  routerPath?: string;
+interface Layer {
+  name: string;
+  route?: Route;
 }
 
 interface ExpressApp {
-  _router: {
-    stack: Stack[];
-  };
+  // Express 3
+  routes?: Record<string, Route[][]>;
+  // Express 4
+  _router?: { stack: Layer[] };
+  // Express 4, 5 Router
+  stack?: Layer[];
+  // Express 5
+  router?: { stack: Layer[] } | string;
 }
 
 declare function expressListRoutes(app: ExpressApp, options?: Options): Array<{ method: string; path: string }>;
 
-export { expressListRoutes as default };
+export = expressListRoutes;
